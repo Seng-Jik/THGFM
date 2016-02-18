fd5a5f514b1a064940b15e34ccd620146587e58f
星翼 <853974536@qq.com>
2016-02-18 16:20:15 +0800

    存档管理器

---
+++ include/GameDataMgr.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Snow.h"
+#include <cstdio>
+
+/** 存档管理器 */
+
+class GameDataMgr{
+public:
+    void ReadInitSetting(Uint8* gamepad);   //读取基本设置
+    //gamepad设置为Uint8数组，共八个数字，前四个分别为1P的SHOT,BOOM,SLOW,PAUSE的手柄键值，后四个为2P的。
+    void SetGamePad(Uint8* gamepad);
+    //格式同上，设置手柄映射并应用到文件
+};
+
+extern GameDataMgr gameData;
+
+/** 数据文件格式 *
+ * PAW02.sav
+ * 前8字节为Uint8的手柄映射
+ * 
+**/
