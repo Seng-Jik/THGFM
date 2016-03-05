49ce40d007024ce317791a6428fdf90c5d7e5556
星翼 <853974536@qq.com>
2016-03-05 22:18:47 +0800

    标题界面选单活动基本完成。

--- include/Title.h
+++ include/Title.h
@@ -2,9 +2,10 @@
 #define TITLE_H
 
 #include "Snow.h"
+#include "KeyMapAct.h"
 #include <vector>
 
-class Title : public Snow::Activity
+class Title : public KeyMapAct
 {
     public:
         Title();
@@ -13,6 +14,7 @@ class Title : public Snow::Activity
         void OnNext();
         void OnDraw();
         void OnHide();
+        void OnEvent(int,Key,bool);
     protected:
     private:
 
@@ -34,14 +36,17 @@ class Title : public Snow::Activity
             ButtonWork btn;
         };
         enum{
-            SHOWING,NORMAL,HIDING
+            SHOWING,NORMAL,HIDING,SELECTER_MOVING
         }m_buttonState;
         std::vector<Button> m_buttons;
-        int m_actButton,m_btn_y;
+        Uint16 m_actButton,m_btn_y;
         Snow::Timer m_btn_tmr;
+        Snow::Sprite m_selecter;
+        int m_selecter_trg,m_selecter_old;  //Value Y
         void clearButton();
         void addButton(const char* file,ButtonWork);
         void doButton(ButtonWork);
+        void setActButton(int);
         void showButton();
         void hideButton();
         void onNextButton();
--- src/Title.cpp
+++ src/Title.cpp
@@ -1,5 +1,7 @@
 #include "Title.h"
 #include "Tools.h"
+#include "MathFunc.h"
+#include <Snow/Debug.h>
 
 Title::Title()
 {
@@ -9,15 +11,16 @@ Title::Title()
 
 void Title::OnInit()
 {
-
+    m_selecter.Load("Title/Selecter.png");
+    m_actButton = 0;
 }
 
 void Title::OnShow()
 {
-    m_actButton = 0;
-    m_btn_y = 20;
+    m_btn_y = 200;
     addButton("Title/Start.png",START);
-    addButton("Title/Start.png",START);
+    addButton("Title/Start.png",STARTEXTRA);
+    addButton("Title/Start.png",EXIT);
     showButton();
 }
 
@@ -41,6 +44,7 @@ void Title::clearButton()
     for(Button& b:m_buttons)
         SDL_DestroyTexture(b.tex);
     m_buttons.clear();
+    m_btn_y = 200;
 }
 
 void Title::addButton(const char* file, ButtonWork work)
@@ -49,54 +53,170 @@ void Title::addButton(const char* file, ButtonWork work)
     b.btn = work;
     b.tex = LoadPic(file);
     SDL_QueryTexture(b.tex,nullptr,nullptr,&b.dst_draw.w,&b.dst_draw.h);
-    b.dst.x = 100;
+    b.dst_draw.x = b.dst.x = 100;
     b.dst.y = m_btn_y;
     m_btn_y+=b.dst_draw.h;
     b.alpha = 255;
     m_buttons.push_back(b);
 }
 
-void Title::doButton(ButtonWork)
+void Title::doButton(ButtonWork b)
 {
-
+    switch(b){
+    case EXIT:
+        Snow::Exit(0);break;
+    case START:
+        break;
+    case STARTEXTRA:
+        break;
+    case PRATCICE:
+        break;
+    case RANK:
+        break;
+    case GAMEPADKEYSET:
+        break;
+    case MUSICROOM:
+        break;
+    }
 }
 
 void Title::showButton()
 {
     m_buttonState = SHOWING;
     m_btn_tmr.Reset();
+    for(Button& b:m_buttons)
+            b.dst_draw.y = 150;
+    m_selecter_trg = m_buttons[m_actButton].dst.y;
 }
 
 void Title::hideButton()
 {
-
+    m_buttonState = HIDING;
+    m_btn_tmr.Reset();
 }
 
 void Title::onNextButton()
 {
+    if(m_buttonState == SHOWING){
+        float per = ACGCross::ArcFunc(float(m_btn_tmr.GetTimer())/500);
+        if(per == -1){
+            for(Button& b:m_buttons){
+                b.dst_draw.y = b.dst.y;
+                b.alpha = 192;
+            }
+            m_buttonState = NORMAL;
+            m_selecter.SetPos(100,m_selecter_trg);
+            m_selecter.SetAlpha(255);
+        }
+        else{
+            for(Button& b:m_buttons){
+                b.dst_draw.y = 150 + (per*b.dst.y - 150);
+                b.alpha = 192*per;
+            }
+            m_selecter.SetPos(100,int(per*(m_selecter_trg - 50)+50));
+            m_selecter.SetAlpha(255*per);
+        }
+    }
 
+    else if(m_buttonState == HIDING){
+        float per = ACGCross::ArcFunc(float(m_btn_tmr.GetTimer())/500);
+        if(per == -1){
+            for(Button& b:m_buttons){
+                b.dst_draw.y = 150;
+                b.alpha = 0;
+            }
+            m_buttonState = NORMAL;
+            m_selecter.SetAlpha(0);
+            //Do Work
+            doButton(m_buttons[m_actButton].btn);
+        }
+        else if(per >0 && per <1) for(Button& b:m_buttons){
+            b.dst_draw.y = 150 + ((1-per)*b.dst.y - 150);
+            b.alpha = 192-192*per;
+            m_selecter.SetPos(100,int((1-per)*(m_selecter_trg - 50)+50));
+            m_selecter.SetAlpha(255-255*per);
+        }
+    }
+
+    else if(m_buttonState == SELECTER_MOVING){
+        float per = ACGCross::ArcFunc(float(m_btn_tmr.GetTimer())/500);
+        if(per == -1){
+            m_selecter.SetPos(100,int(m_selecter_trg));
+            m_buttonState = NORMAL;
+        }else{
+            m_selecter.SetPos(100,int(per*(m_selecter_trg - m_selecter_old))+m_selecter_old);
+        }
+    }
 }
 
 void Title::selectUp()
 {
-
+    if(m_actButton > 0){
+        int x;
+        m_selecter.GetPos(x,m_selecter_old);
+        --m_actButton;
+        m_selecter_trg = m_buttons[m_actButton].dst.y;
+        m_buttonState = SELECTER_MOVING;
+        m_btn_tmr.Reset();
+    }
 }
 
 void Title::selectDown()
 {
+    if(m_actButton < m_buttons.size() -1){
+        int x;
+        m_selecter.GetPos(x,m_selecter_old);
+        ++m_actButton;
+        m_selecter_trg = m_buttons[m_actButton].dst.y;
+        m_buttonState = SELECTER_MOVING;
+        m_btn_tmr.Reset();
+    }
+}
 
+void Title::setActButton(int i){
+    int x;
+    m_selecter.GetPos(x,m_selecter_old);
+    m_actButton=i;
+    m_selecter_trg = m_buttons[m_actButton].dst.y;
+    m_buttonState = SELECTER_MOVING;
+    m_btn_tmr.Reset();
 }
 
 void Title::selectEnter()
 {
-
+    hideButton();
 }
 
 void Title::onDrawButton()
 {
     for(Button& b:m_buttons){
-        SDL_Rect dst={b.dst.x,b.dst.y,b.dst_draw.w,b.dst_draw.h};
+        //SDL_Rect dst={b.dst.x,b.dst_draw.y,b.dst_draw.w,b.dst_draw.h};
         SDL_SetTextureAlphaMod(b.tex,b.alpha);
-        SDL_RenderCopy(Snow::pRnd,b.tex,nullptr,&dst);
+        SDL_RenderCopy(Snow::pRnd,b.tex,nullptr,&b.dst_draw);
+    }
+    m_selecter.OnDraw();
+}
+
+void Title::OnEvent(int, Key k, bool b)
+{
+    if(m_buttonState == NORMAL || m_buttonState == SELECTER_MOVING){
+        if(b){
+            switch(k){
+            case T_UP:
+                selectUp();
+                break;
+            case T_DOWN:
+                selectDown();
+                break;
+            case T_ENTER:
+            case T_SHOT:
+                selectEnter();
+                break;
+            /* Debug */
+            case T_ESC:
+                showButton();
+            }
+        }
     }
 }
+
